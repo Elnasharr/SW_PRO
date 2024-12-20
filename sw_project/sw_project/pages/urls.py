bb0d456bc4e485f80112d03776e93e4a942d3c9f
Fadwa Hany <142605066+Fadwahanyy@users.noreply.github.com>
2024-12-21 00:10:03 +0200

    Add files via upload

---
+++ sw_project/sw_project/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('universities/', views.universities, name='universities'),  
+    path('universities/<int:university_id>/', views.university_details, name='university_details'),  
+    path('scholarships/', views.scholarships, name='scholarships'), 
+    path('scholarships/<int:scholarship_id>/', views.scholarship_details, name='scholarship_details'),  
+   
+]
