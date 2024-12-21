2140dd05897d1b4c273344a9f9471c0b4f0449e0
fadwa <fadwahany2030@gmail.com>
2024-12-21 13:51:48 +0200

    View Details is conected

--- sw_project/pages/urls.py
+++ sw_project/pages/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('universities/', views.universities, name = 'universities'),
-    path('scholarships/', views.scholarships, name = 'scholarships'),
-    path('universities/university_details/<int:id>', views.university_details, name = 'university_details'),
-    path('scholarships/scholarship_details/<int:id>', views.scholarship_details, name = 'scholarship_details'),
+    path('', views.index, name='index'),
+    path('universities/', views.universities, name='universities'),  
+    path('universities/<int:university_id>/', views.university_details, name='university_details'),  
+    path('scholarships/', views.scholarships, name='scholarships'), 
+    path('scholarships/<int:scholarship_id>/', views.scholarship_details, name='scholarship_details'),  
+   
 ]
+#last
