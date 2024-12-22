67846e3749a75a06d9c6cdef4fc3ec3ea6b748cd
Elnasharr <a.khaled2261@nu.edu.eg>
2024-12-22 02:07:48 +0200

    Integrated the website with the database.

--- sw_project/sw_project/settings.py
+++ sw_project/sw_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'sw_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB',  
+        'USER': 'root',           
+        'PASSWORD': '12345678',   
+        'HOST': 'localhost',          
+        'PORT': '3306',                
     }
 }
 
